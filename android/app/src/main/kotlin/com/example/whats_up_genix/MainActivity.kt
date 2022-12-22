d03258265224d67104f91dd7bcc554007313cbcb
SoniaEjaz-sonia <sejaz2510@gmail.com>
2022-12-23 01:38:53 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/whats_up_genix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.whats_up_genix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkTealGreenColor = Color(0XFF075E54);
+  static const Color lightGreenColor = Color(0XFF25D366);
+  static const Color tealGreenColor = Color(0XFF128C7E);
+  static const Color blueColor = Color(0XFF34B7F1);
+  static const Color whiteColor = Colors.white;
+}
