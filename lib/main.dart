d682d1238ed186bd731c3f861bc646eaf1e441af
SoniaEjaz-sonia <sejaz2510@gmail.com>
2022-12-23 13:04:22 +0800

    home tabs work in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whats_up_genix/constants/colors.dart';
+import 'package:whats_up_genix/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,60 +17,12 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData().copyWith(
         primaryColor: AppColors.darkTealGreenColor,
+        secondaryHeaderColor: AppColors.tealGreenColor,
         appBarTheme: const AppBarTheme(
           backgroundColor: AppColors.darkTealGreenColor,
         ),
       ),
       home: const WhatsUpHome(title: 'WhatsUp'),
-    );
-  }
-}
-
-class WhatsUpHome extends StatefulWidget {
-  final String title;
-
-  const WhatsUpHome({Key? key, required this.title}) : super(key: key);
-
-  @override
-  State<WhatsUpHome> createState() => _WhatsUpHomeState();
-}
-
-class _WhatsUpHomeState extends State<WhatsUpHome> with SingleTickerProviderStateMixin {
-  late TabController tabController;
-
-  @override
-  void initState() {
-    tabController = TabController(length: 4, vsync: this, initialIndex: 1);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.7,
-        title: Text(
-          widget.title,
-          style: const TextStyle(fontSize: 20),
-        ),
-        actions: const [
-          Icon(Icons.camera_alt_outlined),
-          SizedBox(width: 20),
-          Icon(Icons.search),
-          SizedBox(width: 20),
-          Icon(Icons.more_vert),
-        ],
-        bottom: TabBar(
-          controller: tabController,
-          indicatorColor: AppColors.whiteColor,
-          tabs: const [
-            Tab(icon: Icon(Icons.people)),
-            Tab(text: "Chats"),
-            Tab(text: "Status"),
-            Tab(text: "Calls"),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:whats_up_genix/constants/colors.dart';
+import 'package:whats_up_genix/screens/calls_screen.dart';
+import 'package:whats_up_genix/screens/chats_screen.dart';
+import 'package:whats_up_genix/screens/community_screen.dart';
+import 'package:whats_up_genix/screens/status_screen.dart';
+
+class WhatsUpHome extends StatefulWidget {
+  final String title;
+
+  const WhatsUpHome({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<WhatsUpHome> createState() => _WhatsUpHomeState();
+}
+
+class _WhatsUpHomeState extends State<WhatsUpHome> with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 4, vsync: this, initialIndex: 1);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.7,
+        title: Text(
+          widget.title,
+          style: const TextStyle(fontSize: 23),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.camera_alt_outlined),
+          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+        ],
+        bottom: TabBar(
+          controller: tabController,
+          indicatorColor: AppColors.whiteColor,
+          tabs: const [
+            Tab(icon: Icon(Icons.people)),
+            Tab(text: "Chats"),
+            Tab(text: "Status"),
+            Tab(text: "Calls"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: const [
+          CommunityScreen(),
+          ChatsScreen(),
+          StatusScreen(),
+          CallsScreen(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).secondaryHeaderColor,
+        onPressed: () {},
+        child: const Icon(Icons.message),
+      ),
+    );
+  }
+}
